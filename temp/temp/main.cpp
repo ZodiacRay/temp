d2ddfcae915b0424f94cabf36b998e31b8103645
minsung <99423006+zarame3636@users.noreply.github.com>
2025-12-12 19:49:04 +0900

    change-01

--- temp/temp/main.cpp
+++ temp/temp/main.cpp
@@ -2,10 +2,14 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cout << "Changed!" << "\n"; 
+
+    return 0;
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
